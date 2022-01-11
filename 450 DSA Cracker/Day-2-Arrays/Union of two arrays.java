3577046538f91ee53060e4ed0b2dc3d0647ef092
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-01-11 13:32:16 +0530

    Create Union of two arrays.java

---
+++ 450 DSA Cracker/Day-2-Arrays/Union of two arrays.java
@@ -0,0 +1,14 @@
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        //Your code here
+        HashSet<Integer> s = new HashSet<>(); 
+        for(int i=0;i<a.length;i++){
+            s.add(a[i]);
+        }
+        for(int j=0;j<b.length;j++){
+            s.add(b[j]);
+        }
+        return s.size();
+    }
+}
