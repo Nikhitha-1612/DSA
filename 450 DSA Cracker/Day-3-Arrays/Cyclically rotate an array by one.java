7f87a7bf7d1d1e5113116f558f7b672059340467
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-01-13 22:56:42 +0530

    Create Cyclically rotate an array by one.java

---
+++ 450 DSA Cracker/Day-3-Arrays/Cyclically rotate an array by one.java
@@ -0,0 +1,12 @@
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int x = arr[arr.length-1], i;
+       for (i = arr.length-1; i > 0; i--){
+          arr[i] = arr[i-1];
+       
+    }
+    arr[0] = x;
+    }
+}
