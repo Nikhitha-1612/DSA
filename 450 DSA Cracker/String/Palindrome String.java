ae84efdae2580a5460eaa777eacc8c2d705db7a2
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-06-15 09:54:08 +0530

    Create Palindrome String.java

---
+++ 450 DSA Cracker/String/Palindrome String.java
@@ -0,0 +1,15 @@
+//https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+class Solution {
+    int isPalindrome(String S) {
+        // code here
+        int left =0;
+        int right=S.length()-1;
+        while(left<right){
+            if(S.charAt(left)!=S.charAt(right))
+            return 0;
+            left++;
+            right--;
+        }
+        return 1;
+    }
+};
